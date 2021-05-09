97ca9fc4f43a6907fdbdce9918c3dbae3a199c32
iamyogesh21 <singhyogesh2104>
2021-05-09 10:20:58 +0530

    Update sophia.py

--- sophia.py
+++ sophia.py
@@ -1,29 +1,79 @@
-import pyttsx3
-import speech_recognition as sr
-# import time
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import time
+import wikipedia #pip install wikipedia
+# import webbrowser
+import os
 
 assistant = pyttsx3.init('sapi5')
-voice = assistant.getProperty('voices')
-# print(voice)
-assistant.setProperty('voices',voice[0].id)
+voices = assistant.getProperty('voices')
+# print(voices[1].id)
+assistant.setProperty('voice', voices[0].id)
+
+
 def speak(audio):
     assistant.say(audio)
     assistant.runAndWait()
-    print(f"You Say : {audio}")
 
-# speak("Hello sir how can i help")
-def recognize():
+def takeCommand():
     command = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening.....................")
-        command.pause_threshold=1
+        print("Listening...")
+        # print(sr.Mircrophone.list_microphone_name()) # also pass device_index=1,2,3,0 etc
+        command.pause_threshold = 1
         audio = command.listen(source)
+
     try:
-        print("Recognizing.......")
-        key_word = command.recognize_google(audio, language='en-US')
-        print(f"You say : {key_word}")
-    except Exception as Error:
+        print("Recognizing...")    
+        query = command.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:  
+        print("Say that again please...")  
         return "None"
-    return key_word.lower()
-query = recognize()
-# speak("Hello")
+    return query
+
+
+    
+def startup():
+    speak("Initializing Sophia")
+    speak("Starting all systems applications")
+    speak("Installing and checking all drivers")
+    speak("Checking the internet connection")
+    speak("All drivers are up and running")
+    speak("All systems have been activated")
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<=12:
+        speak("Good Morning")
+    elif hour>12 and hour<18:
+        speak("Good afternoon")
+    else:
+        speak("Good evening")
+    strTime  =datetime.datetime.now().strftime('%H:%M:%S')
+    speak(f"Currently it is {strTime}")
+    speak("I am Jarvis. Online and ready sir. Please tell me how may I help you")
+
+
+if __name__ == "__main__":
+    startup()
+    while True:
+        query = takeCommand().lower()
+        # Logic for executing tasks based on query
+        
+        if "hello" in query:
+            speak("hello sir how can i help.")
+        elif "how are you" in query:
+            speak("I am fine sir What about you")
+        elif "fine" in query:
+            speak("ok, sir")
+        elif "bye" in query:
+            speak("bye sir, you can call me any time")
+            break
+        elif 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
